3212d93d69f8d37b3225749ac9ca1c297c88ee4a
Riya <riyacharhekar10@gmail>
2026-04-03 14:00:46 +0530

    added s3 backup

--- terraform/backup.tf
+++ terraform/backup.tf
@@ -9,13 +9,14 @@ terraform {
     
   }
 }
-resource "aws_instance" "myinstance" {
+resource "aws_instance" "myinstance2" {
     ami = "ami-01b14b7ad41e17ba4"
     instance_type = "t3.micro"
     key_name = "tf-1"
     vpc_security_group_ids = [ "sg-0e4535a3abb5aba8a" ]
     tags = {
         name = "Riya"
+        Name = "Riya"
         Environment = "Dev"
     }
 }
