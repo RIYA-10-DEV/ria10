481d556f862e4f680e608b25b22052cbfb988606
Riya <riyacharhekar10@gmail>
2026-04-09 12:13:39 +0530

    instance add

--- terraform/instance.tf
+++ terraform/instance.tf
@@ -1,13 +1,13 @@
 provider "aws" {
     region = "us-east-1"
 }
-resource "aws_instance" "myinstance" {
+resource "aws_instance" "myinstance1" {
     ami = "ami-01b14b7ad41e17ba4"
     instance_type = "t3.micro"
     key_name = "tf-1"
     vpc_security_group_ids = [ "sg-0e4535a3abb5aba8a" ]
     tags = {
-        name = "Riya"
+        name = "Ria"
         Environment = "Dev"
     }
 }
