adf732d16ff387ec58ee8dc69f65afc7d5cc2a1a
Riya <riyacharhekar10@gmail>
2026-04-09 13:23:21 +0530

    variable added

---
+++ terraform/variable.tf
@@ -0,0 +1,19 @@
+variable "ami-id" {
+    default = "ami-0ea87431b78a82070"
+    description = "us-east-1"
+}
+variable "instance_type" {
+    default = "t3.micro"
+}
+variable "key_name" {
+    default = "tf-1"
+}
+variable "mysg" {
+    default = "sg-0e4535a3abb5aba8a"
+}
+variable "rg" {
+    default = "us-east-1"
+}
+variable "availability_zone" {
+    default = "us-east-1"
+}
