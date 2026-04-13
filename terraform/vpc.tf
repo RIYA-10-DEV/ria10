609729bf2406dda359119bbc293677318bea185d
Riya <riyacharhekar10@gmail>
2026-04-13 12:25:59 +0530

    vpc added

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -1,7 +1,7 @@
 provider "aws" {
     region = "ap-south-1"
-  
 }
+
 resource "aws_vpc" "myvpc" {
     cidr_block = "10.0.0.0/16"
     tags = {
@@ -9,8 +9,8 @@ resource "aws_vpc" "myvpc" {
       Name = "Riya"
       env = "dev"
     }
-  
 }
+
 resource "aws_subnet" "pub-sub" {
     vpc_id = aws_vpc.myvpc.id
     cidr_block = "10.0.0.0/24"
