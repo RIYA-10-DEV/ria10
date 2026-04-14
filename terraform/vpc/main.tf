446002e79a713e7d6cf12334d5af023ae1f8018d
Riya <riyacharhekar10@gmail>
2026-04-14 14:26:27 +0530

    added vpc

--- terraform/vpc/main.tf
+++ terraform/vpc/main.tf
@@ -56,7 +56,7 @@ resource "aws_route_table_association" "public_assoc" {
 resource "aws_route" "public_internet" {
     route_table_id = aws_route_table.public_route.id
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.my_igw.owner_id
+    gateway_id = aws_internet_gateway.my_igw.id
 }
 resource "aws_route_table_association" "private_assoc" {
     subnet_id = aws_subnet.private_subnet.id
